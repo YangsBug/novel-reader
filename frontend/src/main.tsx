264d692ffe78b0c9d63fd86f13fd90f524abea24
YangsBug <2412680481@qq.com>
2026-07-01 13:11:04 +0800

    github pages deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
@@ -21,7 +21,7 @@ document.documentElement.className = `theme-${theme || 'light'}`
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <HashRouter>
         <App />
         <ChatBot />
         <Toaster
@@ -34,7 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             }
           }}
         />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
